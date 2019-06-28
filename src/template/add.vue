0280fc6ca0c07ea7be74136c135a6d9d214e76a9
Varsha <varsha.mobilyte@outlook.com>
2019-06-29 04:58:32 +0530

    localStorage

--- src/template/add.vue
+++ src/template/add.vue
@@ -1,48 +1,56 @@
 <template>
   <div class="add">
     <div class="formlayout">
+      <div class="form-feild" v-if="updateButton"> 
       <h2>
-        <Strong>{{ msg }}</strong>
+      <Strong>{{ msg1 }}</strong>
       </h2>
-      <form @submit.prevent="onAdd()" method="post" class="form">
+      </div>
+      <div class="form-feild" v-if="!updateButton"> 
+      <h2>
+      <Strong>{{ msg }}</strong>
+      </h2>
+      </div>
+      <form @submit.prevent="onAdd()" method="post" class="form" >
         <div class="form-feild">
           <input class="input-feild" type="text" placeholder="First Name" v-model="form.firstName" autocomplete/>
-          <!-- <span class="error">{{firstName}}</span> -->
         </div>
         <div class="form-feild">
           <input class="input-feild" type="text" placeholder="Last Name" v-model="form.lastName" autocomplete/>
-          <!-- <span class="error">{{lastName}}</span> -->
         </div>
         <div class="form-feild">
           <input class="input-feild" type="number" placeholder="Age" v-model="form.age" autocomplete/>
-          <!-- <span class="error">{{age}}</span> -->
         </div>
-        <button type="submit" class="bt btn-info">Add</button>
-        &nbsp;
-        <button type="button" class="bt btn-info" v-on:click="update()" >Update</button>
-         <!-- <button type="button" v-on:click="onUpdate()" class="bt btn-info">Update</button> -->
+        <div class="form-feild" v-if="!updateButton" >
+          <button type="submit" class="bt btn-info">Add</button> &nbsp;
+        </div>
+        <div class="form-feild" v-if="updateButton" >
+          <button type="button"  v-on:click="update()" class="bt btn-info">Update</button> &nbsp;
+        </div>
       </form>
     </div>
-    <form class="form" v-if="show" method="post" >
+
+    <form class="form" v-if="show" method="post">
       <table style="width:100%">
         <tr>
+          <!-- <th>Email</th> -->
           <th>Firstname</th>
           <th>Lastname</th>
           <th>Age</th>
         </tr>
-        <tr v-for="(ele, index) in array1">
+        <tr v-for="(ele, index) in localSto">
+          <!-- <td>{{ele.email}}</td> -->
           <td>{{ele.firstName}}</td>
           <td>{{ele.lastName}}</td>
           <td>{{ele.age}}</td>
-           <button  type="button" v-on:click="onUpdate(index)"  class="rounded-0" variant="success">Update</button>
-          &nbsp;
-        <button  type="button" v-on:click="onDelete(index)"  class="rounded-0" variant="success">Delete</button>
+          <button type="button" v-on:click="onUpdate(index)" class="rounded-0" variant="success">Update</button> &nbsp;
+          <button type="button" v-on:click="onDelete(index)" class="rounded-0" variant="success">Delete</button>
         </tr>
         <br>
       </table>
     </form>
+
   </div>
-  
 </template>
 
 <script>
@@ -51,66 +59,91 @@
     data() {
       return {
         msg: "Add",
+          msg1: "Update",
         form: {
+          // email: "",
           firstName: "",
           lastName: "",
           age: ""
         },
         array1: [],
         indexOfArray: "",
-        show: false
-        
+        show: true,
+        updateButton: false,
+        localSto: JSON.parse(localStorage.getItem("user"))
       };
     },
     methods: {
-      goBack() {
-        window.history.length > 1 ? this.$router.go(-1) : this.$router.push("/");
-      },
+            myFunctionOnLoad(){
+           this.array1 = this.array1.concat(this.localSto);
+        },
       onAdd() {
-        this.array1.push({firstName: this.form.firstName,
-                          lastName: this.form.lastName,
-                          age: this.form.age});
-     
+        if(this.form.firstName!= '')
+        {
+        this.array1.push({
+          // email: this.form.email,
+          firstName: this.form.firstName,
+          lastName: this.form.lastName,
+          age: this.form.age
+        });
         if (this.array1.length) {
+          localStorage.setItem("user", JSON.stringify(this.array1));
+           localStorage.setItem("user1", JSON.stringify(this.form));
           this.show = true;
         }
-
-        alert(this.form.firstName);
-        this.form= {
+        }
+        console.log("SHOW VALUE", this.array1);
+        this.localSto = JSON.parse(localStorage.getItem("user"));
+        console.log(this.localSto);
+        // alert(this.form.firstName);
+        this.form = {
+          // email: " ",
           firstName: "",
           lastName: "",
           age: ""
-        }
+        };
       },
-
-      onUpdate(index)
-      {
-        console.log("IndexOne");
-         this.indexOfArray=index;
-        this.form= {
-          firstName: this.array1[index].firstName,
+  
+      onUpdate(index) {
+        this.updateButton= true;
+        console.log("IndexOne",this.updateButton);
+        this.indexOfArray = index;
+        this.form = {
+          //  email: this.array1[index].email,
+           firstName: this.array1[index].firstName,
           lastName: this.array1[index].lastName,
           age: this.array1[index].age
-        }
-       console.log("IndexOfArray", this.indexOfArray)
-
+        };
+        //  localStorage.setItem("user", this.array1);
+        console.log("IndexOfArray", this.indexOfArray);
       },
-      update()
-      {
+  
+      update() {
         // console.log(indexOfArray);
-        console.log(this.form.firstName)
-       this.array1[this.indexOfArray].firstName=this.form.firstName,
-        this.array1[this.indexOfArray].lastName= this.form.lastName,
-        this.array1[this.indexOfArray].age=this.form.age
+        // console.log(this.form.firstName);
+        //  this.array1 = this.array1.concat(this.localSto);
+        // (this.array1[this.indexOfArray].email = this.form.email),
+        (this.array1[this.indexOfArray].firstName = this.form.firstName),
+        (this.array1[this.indexOfArray].lastName = this.form.lastName),
+        (this.array1[this.indexOfArray].age = this.form.age);
+       
+        localStorage.setItem("user", JSON.stringify(this.array1));
+        this.localSto = JSON.parse(localStorage.getItem("user"));
+        console.log(this.localSto.length);
+        //  this.updateButton= false;
       },
-      onDelete(index)
-      {
-        console.log("bdfjhsfs")
-        this.array1.splice(index, 1);
+  
+      onDelete(index) {
+        console.log("Index",index)
+        localStorage.setItem("user", JSON.stringify(this.array1));
+        this.localSto = JSON.parse(localStorage.getItem("user"));
+         console.log("sdfjsdfhsdhj",this.localSto);
       }
-
-    }
-  };
+    },
+      created: function(){
+        this.myFunctionOnLoad()
+      }
+  }
 </script>
 
 <style>
